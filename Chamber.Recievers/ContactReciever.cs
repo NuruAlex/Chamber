4a4f28cc888fd3217f562682b6ab87b194e5f046
NuruAlex <nurualeksej@gmail.com>
2024-03-20 14:53:27 +0300

    asd

--- Chamber.Recievers/ContactReciever.cs
+++ Chamber.Recievers/ContactReciever.cs
@@ -2,6 +2,9 @@
 using Chamber.Core.Users;
 using Chamber.Recievers.Args;
 using Events;
+using Messages.Core.Reply.Buttons;
+using Messages.Core.Reply.Markups;
+using Messages.Core.Reply.Rows;
 using Messages.Core.Types;
 using Messages.Senders;
 using Telegram.Bot.Types;
@@ -18,8 +21,23 @@ public class ContactReciever
     private async void ContacRecieved(ContactRecievedArgs args)
     {
         long chat = args.ChatId;
+
         Contact contact = args.Contact;
+
         DataBase.Users.Add(new Client(chat, contact.PhoneNumber, contact.FirstName));
-        await Sender.SendMessage(new TextMessage(args.ChatId, $"{contact.FirstName}, вы были успешно зарегестрированы"));
+
+        await Sender.SendMessage(new TextMessage(args.ChatId, $"{contact.FirstName}, вы были успешно зарегестрированы")
+        {
+            Markup = new RemoveMarkup()
+        });
+
+        await Sender.SendMessage(new TextMessage(chat, "Выбрите тип проблемы")
+        {
+            Markup = new InlineMarkup(
+                new InlineButton("Не типовая", "1"), new InlineRow(),
+                new InlineButton("Замена бланка", "2"), new InlineRow(),
+                new InlineButton("Рец не готов", "3")
+                )
+        });
     }
 }
